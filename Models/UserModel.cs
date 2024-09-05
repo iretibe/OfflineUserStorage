17cb042e4ca22c1846cdfc18d2b0c80219a6dccc
Somad Yessoufou <yessouf2009@yahoo.fr>
2024-09-05 19:01:47 +0000

    Added Blazor Standalone User Application

---
+++ Models/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace OfflineUserStorage.Models
+{
+    public class UserModel
+    {
+        public DateTime CreateDate { get; set; } = DateTime.UtcNow;
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
